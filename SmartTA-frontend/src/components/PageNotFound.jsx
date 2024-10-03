140feb7eaeef31ac1b2425b68390d1159bf683f7
MianHadihehe <mianhadi.01@gmail.com>
2024-10-04 02:17:31 +0500

    404 page fixed

--- SmartTA-frontend/src/components/PageNotFound.jsx
+++ SmartTA-frontend/src/components/PageNotFound.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
-import '../styling/pagenotfound.css'; // Ensure you have a CSS file for this page
+import { useNavigate, Link } from 'react-router-dom';
+import '../styling/pagenotfound.css'; 
+
 
 const splineURL = import.meta.env.VITE_BABYROBO_URL;
 
 const PageNotFound = () => {
+  const navigate = useNavigate();
   return (
     <div className="pagenotfound-main-cont">
       <div className="box1">
@@ -11,13 +14,14 @@ const PageNotFound = () => {
           src={splineURL}
           id="babyrobo"
           width="100%"
-          height="55%"
+          height="100%"
         ></iframe>
+        <div className="page-not-found-temp"></div>
       </div>
       <div className="pagenotfound-container">
         <h1>ERROR 404 !</h1>
         <p>Oops! The page you're looking for doesn't exist.</p>
-        <a href="/" className="home-btn">Go Back Home</a>
+        <Link to="/" className="home-btn">Go Back Home</Link>
       </div>
       <div className="box2"></div>
     </div>
